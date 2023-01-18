66c7d721a3eefb18bdd2e734ea6455b6ccba77e8
WilliamFancyS0n <WilliamAlger222@gmail.com>
2023-01-17 19:18:33 -0500

    git init

---
+++ Riki/config.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = 'images'
+CONTENT_DIR = 'content'
+USER_DIR = 'user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
+#PRIVATE = False
