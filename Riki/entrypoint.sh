b612cb9c28771b58271b51e3e572728cc7092a4a
WilliamFancyS0n <WilliamAlger222@gmail.com>
2023-03-14 19:55:12 -0400

    fixed entrypoint.sh docs command

--- Riki/entrypoint.sh
+++ Riki/entrypoint.sh
@@ -7,10 +7,10 @@ case ${1} in
     debug)
         python3 Riki.py
         ;;
-    *)
-        exec "$@"
-        ;;
     docs)
         pdoc3 --html --output-dir=/opt/app/wiki/web/static/docs --force wiki
+        ;;
+    *)
+        exec "$@"
         ;;
 esac
