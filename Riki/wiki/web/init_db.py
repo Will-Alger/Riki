f4cc16cb30dda423a46c517241866f64e1392c20
WilliamFancyS0n <WilliamAlger222@gmail.com>
2023-03-21 20:28:34 -0400

    Updated init_db.py to only run if riki.db !exist

--- Riki/wiki/web/init_db.py
+++ Riki/wiki/web/init_db.py
@@ -1,32 +1,39 @@
 import sqlite3
+import os
 
-#TODO only init if riki.db doesn't exist in /var/db
+dbpath = "/var/db/riki.db"
 
-# how to make connection string to var/db
-connection = sqlite3.connect("/var/db/riki.db")
+if (not os.path.exists(dbpath)):
+    print(" * database not found, running int_db.py")
+    #TODO only init if riki.db doesn't exist in /var/db
 
-with open('wiki/web/schema.sql') as f:
-    connection.executescript(f.read())
+    # how to make connection string to var/db
+    connection = sqlite3.connect("/var/db/riki.db")
 
-cur = connection.cursor()
+    with open('wiki/web/schema.sql') as f:
+        connection.executescript(f.read())
 
-cur.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            ('john doe', 'john.doe@example.com', 'password123')
-            )
+    cur = connection.cursor()
 
-cur.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            ('bob smith', 'bob.smith@example.com', 'password789')
-            )
+    cur.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+                ('john doe', 'john.doe@example.com', 'password123')
+                )
+
+    cur.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+                ('bob smith', 'bob.smith@example.com', 'password789')
+                )
 
 
 
-cur = connection.cursor()
-cur.execute("SELECT * FROM users")
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM users")
 
-rows = cur.fetchall()
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-connection.commit()
-connection.close()
+    connection.commit()
+    connection.close()
+else:
+    print(" * database found, skipping init_db.py")
