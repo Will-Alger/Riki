ae625fc7cb4ea5eb97723b01039e100d85e8773e
William Alger <35159547+WilliamFancyS0n@users.noreply.github.com>
2023-03-23 21:09:07 -0400

    added sample database unit tests

--- Riki/tests/test_db.py
+++ Riki/tests/test_db.py
@@ -1,38 +1,40 @@
-
-import pytest
 from Riki import app
-
+import wiki.web.init_db
+from wiki.web.init_db import init_db
 import os
 import tempfile
 import unittest
+import sqlite3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pytest
 
+@pytest.fixture
+def client():
+    db_fd, db_path = tempfile.mkstemp()
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    app.config['TESTING'] = True
+    app.config["PRIVATE"] = False #sets PRIVATE to false to disable user auth (see protect dectorator in users.py)
+    app.config["WTF_CSRF_ENABLED"] = False #disables CSRF in WTForms so that we can simulate posts
 
-class TestInitDB(unittest.TestCase):
+    init_db(db_path, force=True)
+    with app.test_client() as client:
+        client.db_path = db_path # Add the database path as an attribute of the client
+        with app.app_context():
+            pass #if we need stuff in app context we can do it here
+        yield client
+    os.close(db_fd)
+    os.unlink(db_path)
 
-    def setUp(self):
-        self.db_fd, self.db_path = tempfile.mkstemp()
-        self.app = app.test_client()
-        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{self.db_path}'
-        app.config['TESTING'] = True
-        init_db()
+def test_db_file_created(client):
+    db_path = client.db_path
+    assert os.path.exists(db_path), "Database file not created"
 
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(self.db_path)
-
-    def test_db_file_created(self):
-        assert os.path.exists(self.db_path), "Database file not created"
-
-# @pytest.fixture
-# def client(self):
-#     self.db_uri = 'sqlite:///' + os.path.join(basedir, 'test.db')
-#     app.config["TESTING"] = True #sets testing to true, needed for Flask to return proper response
-#     app.config["PRIVATE"] = False #sets PRIVATE to false to disable user auth (see protect dectorator in users.py)
-#     app.config["WTF_CSRF_ENABLED"] = False #disables CSRF in WTForms so that we can simulate posts
-#     with app.test_client() as client:
-#         with app.app_context():
-#             pass #if we need stuff in app context we can do it here
-            
-#         yield client
+def test_users_table_created(client):
+    db_path = client.db_path
+    with sqlite3.connect(db_path) as connection:
+        cur = connection.cursor()
+        cur.execute("PRAGMA table_info('users')")
+        table_info = cur.fetchall()
+        print("Query result:", table_info)
+    assert table_info, "users table not created or empty"
--- Riki/wiki/web/init_db.py
+++ Riki/wiki/web/init_db.py
@@ -1,32 +1,32 @@
 import sqlite3
 import os
 
-def init_db(dbpath='/var/db/riki.db'):
-    if not os.path.exists(dbpath):
-        print(" * database not found, running init_db.py")
+def init_db(dbpath='/var/db/riki.db', force=False):
+    if not os.path.exists(dbpath) or force:
+        print(" * database not found -> running init_db.py")
 
-        # Connect to the database
-        with sqlite3.connect(dbpath) as connection:
+    # Connect to the database
+    with sqlite3.connect(dbpath) as connection:
 
-            # Create tables from schema.sql
-            with open('wiki/web/schema.sql') as f:
-                connection.executescript(f.read())
+        # Create tables from schema.sql
+        with open('wiki/web/schema.sql') as f:
+            connection.executescript(f.read())
 
-            # Insert sample data
-            cur = connection.cursor()
-            cur.executemany("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-                            [('john doe', 'john.doe@example.com', 'password123'),
-                             ('bob smith', 'bob.smith@example.com', 'password789')]
-                            )
+        # Insert sample data
+        cur = connection.cursor()
+        cur.executemany("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+                        [('john doe', 'john.doe@example.com', 'password123'),
+                            ('bob smith', 'bob.smith@example.com', 'password789')]
+                        )
 
-            # Fetch all users and print them
-            cur.execute("SELECT * FROM users")
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+        # Fetch all users and print them
+        cur.execute("SELECT * FROM users")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-            # Commit changes and close the connection
-            connection.commit()
+        # Commit changes and close the connection
+        connection.commit()
 
-    else:
-        print(" * database found, skipping init_db.py")
+    # else:
+        print(" * database found -> skipping init_db.py")
