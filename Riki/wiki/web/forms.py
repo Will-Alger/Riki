febf852f58fbf9f2f071566413f5a9cc07bc6025
teddygizachew <teddygizachew1@gmail.com>
2023-03-24 20:28:37 -0400

    add more fields to sign up form

--- Riki/wiki/web/forms.py
+++ Riki/wiki/web/forms.py
@@ -7,12 +7,16 @@ from wtforms import BooleanField
 from wtforms import TextField
 from wtforms import TextAreaField
 from wtforms import PasswordField
+from wtforms import StringField, validators
 from wtforms.validators import InputRequired
 from wtforms.validators import ValidationError
 
 from wiki.core import clean_url
 from wiki.web import current_wiki
 from wiki.web import current_users
+from flask import flash
+from wiki.web.userDAO import UserDaoManager
+from wiki.web.userDAO import UserDao
 
 
 class URLForm(Form):
@@ -59,14 +63,23 @@ class LoginForm(Form):
 
 class SignupForm(Form):
     name = TextField('', [InputRequired()])
-    email = TextField('', [InputRequired()])
-    password = PasswordField('', [InputRequired()])
+    email = StringField(label='E-mail', validators=[
+        validators.Length(min=5, max=35),
+        validators.Email()
+    ])
+    password = PasswordField('', validators=[
+        validators.Length(min=2, message='Too short'),
+        InputRequired(),
+        validators.EqualTo('confirm_password', message='Passwords must match'),
+    ])
     confirm_password = PasswordField('', [InputRequired()])
 
-    def validate_name(form, field):
-        user = current_users.get_user(field.data)
-        if not user:
-            raise ValidationError('This username does not exist.')
+    def validate_email(form, email):
+        userDaoManager = UserDaoManager('/var/db/riki.db')
+        valemail =  userDaoManager.get_user(email.data)
+        flash(f'val email -> {valemail}')
+        if valemail is not None:
+            raise ValidationError('This Email ID is already registered. Please login')
 
     def validate_password(form, field):
         user = current_users.get_user(form.name.data)
