f81bfcf9619e2a19bcbf25ea069ddf70ddd68e25
teddygizachew <teddygizachew1@gmail.com>
2023-03-24 20:25:05 -0400

    Remove sample data

--- Riki/wiki/web/init_db.py
+++ Riki/wiki/web/init_db.py
@@ -12,19 +12,6 @@ def init_db(dbpath='/var/db/riki.db', force=False):
             with open('wiki/web/schema.sql') as f:
                 connection.executescript(f.read())
 
-            # Insert sample data
-            cur = connection.cursor()
-            cur.executemany("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-                            [('john doe', 'john.doe@example.com', 'password123'),
-                                ('bob smith', 'bob.smith@example.com', 'password789')]
-                            )
-
-            # Fetch all users and print them
-            cur.execute("SELECT * FROM users")
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
-
             # Commit changes and close the connection
             connection.commit()
 
