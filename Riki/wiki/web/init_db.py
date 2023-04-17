a3d597c872e258ecb8b8b105ddbc702b96f0eff5
William Alger <35159547+WilliamFancyS0n@users.noreply.github.com>
2023-04-17 16:03:54 -0400

    commented out init_db.py usages

--- Riki/wiki/web/init_db.py
+++ Riki/wiki/web/init_db.py
@@ -1,19 +1,19 @@
-import sqlite3
-import os
+# import sqlite3
+# import os
 
 
-def init_db(dbpath="/var/db/riki.db", force=False):
-    if not os.path.exists(dbpath) or force:
-        print(" * database not found -> running init_db.py")
+# def init_db(dbpath="/var/db/riki.db", force=False):
+#     if not os.path.exists(dbpath) or force:
+#         print(" * database not found -> running init_db.py")
 
-        # Connect to the database
-        with sqlite3.connect(dbpath) as connection:
-            # Create tables from schema.sql
-            with open("wiki/web/schema.sql") as f:
-                connection.executescript(f.read())
+#         # Connect to the database
+#         with sqlite3.connect(dbpath) as connection:
+#             # Create tables from schema.sql
+#             with open("wiki/web/schema.sql") as f:
+#                 connection.executescript(f.read())
 
-            # Commit changes and close the connection
-            connection.commit()
+#             # Commit changes and close the connection
+#             connection.commit()
 
-    else:
-        print(" * database found -> skipping init_db.py")
+#     else:
+#         print(" * database found -> skipping init_db.py")
