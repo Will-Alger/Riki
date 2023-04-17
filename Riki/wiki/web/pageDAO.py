f2d13c0093179ef42b13d42257ef3e8466b8d631
William Alger <35159547+WilliamFancyS0n@users.noreply.github.com>
2023-04-17 16:03:35 -0400

    added documentation and new method to pageDAO

--- Riki/wiki/web/pageDAO.py
+++ Riki/wiki/web/pageDAO.py
@@ -1,4 +1,3 @@
-import sqlite3
 from wiki.web.db import *
 
 
@@ -8,37 +7,132 @@ class PageDaoManager(object):
         self.cur = self.connection.cursor()
 
     def update_page_index(self, page):
+        """
+        Updates the page_index table for a given page by deleting the old tokens and adding or updating the new ones.
+
+        Args:
+            page (Page): The page object.
+
+        Returns:
+            None
+
+        """
         page_index = page.tokenize_and_count()
+
+        # Delete the old tokens from the page_index table
         self.delete_old_tokens(page, page_index)
+
+        # Add or update the new tokens in the page_index table
         self.add_or_update_tokens(page, page_index)
+
+        # Commit the changes to the database
         self.connection.commit()
 
-    def delete_old_tokens(self, page, page_index):
+    def delete_old_tokens(self, page, new_page_index):
+        """
+        Deletes tokens from the page_index table for a given page that are not included in the new page index.
+
+        Args:
+            page (Page): The page object.
+            page_index (dict): A dictionary containing tokens and their frequencies to be included in the new page index.
+
+        Returns:
+            None
+
+        """
+        # Get the current tokens for the page using the get_tokens method
+        current_index = self.get_tokens(page)
+
+        # Find the tokens to be deleted by getting the difference between the current tokens and the new page index
+        tokens_to_delete = set(current_index.keys()) - set(new_page_index.keys())
+
+        # Check if there are any tokens to delete, if not, return
+        if not tokens_to_delete:
+            return
+
+        # Construct a SQL query to delete tokens that are in tokens_to_delete and match the given page's ID
         self.cur.execute(
             """
-          DELETE FROM page_index
-          WHERE doc_id = ? AND word NOT IN ({})
-      """.format(
-                ", ".join("?" for _ in page_index)
+            DELETE FROM page_index
+            WHERE doc_id = ? AND word IN ({})
+        """.format(
+                ", ".join("?" for _ in tokens_to_delete)
             ),
-            [page.id] + list(page_index.keys()),
+            [page.id] + list(tokens_to_delete),
         )
 
     def add_or_update_tokens(self, page, page_index):
+        """
+        Adds or updates the token frequency in the page_index table for a given page.
+
+        Args:
+            page (Page): The page object.
+            page_index (dict): A dictionary containing tokens and their frequencies
+                                to be added or updated for the given page.
+
+        Returns:
+            None
+
+        """
+        # Iterate over each token and its frequency in the page index
         for token, frequency in page_index.items():
+            # Add or update the token's frequency for the given page in the page_index table
             self.cur.execute(
                 "INSERT OR REPLACE INTO page_index (word, doc_id, frequency) VALUES (?,?,?)",
                 (token, page.id, frequency),
             )
+        # Commit the changes to the database
+        self.connection.commit()
 
     def update_page_index_id(self, new_id, old_id):
-        # Update the "doc_id" value in the "page_index" table for the old page to the ID of the new page
+        """
+        Update the "doc_id" value in the "page_index" table for the old page to the ID of the new page.
+
+        Args:
+            new_id (int): The ID of the new page that will replace the old page's ID.
+            old_id (int): The ID of the old page that will be replaced by the new page's ID.
+
+        Returns:
+            None
+
+        """
+        # Execute the SQL statement to update the "doc_id" value in the "page_index" table.
         self.cur.execute(
             "UPDATE page_index SET doc_id = ? WHERE doc_id = ?", (new_id, old_id)
         )
+
+        # Commit the changes to the database.
         self.connection.commit()
 
+    def get_tokens(self, page):
+        """
+        Retrieves the tokens for a given page from the page_index table in the database.
+
+        :param page: Page object representing the page to retrieve tokens for.
+        :return: Dictionary containing words as keys and their corresponding frequencies as values.
+        """
+        # Query the database for the updated tokens and convert to a dictionary
+        tokens_dict = {
+            word: frequency
+            for word, frequency in self.cur.execute(
+                "SELECT word, frequency FROM page_index WHERE doc_id=?", (page.id,)
+            ).fetchall()
+        }
+        return tokens_dict
+
     def delete(self, page):
+        """
+        This method deletes rows from the page_index table corresponding to
+        the provided page object by using its 'id' attribute.
+
+        Args:
+        - page: The Page object to be deleted from database
+
+        Returns:
+        - None
+        """
         # Remove rows from the page_index table where doc_id = page.id
         self.cur.execute("DELETE FROM page_index WHERE doc_id=?", (page.id,))
+
+        # Commit changes to update the database
         self.connection.commit()
